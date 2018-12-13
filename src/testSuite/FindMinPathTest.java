877a6cccf671d057b15213301aea772bf41bb807
zhangqiang26 <zrY@13145213>
2018-12-13 16:02:28 +0800

    增加测试用例，修复hashmap的hashcode的bug

--- src/testSuite/FindMinPathTest.java
+++ src/testSuite/FindMinPathTest.java
@@ -1,29 +1,192 @@
 package testSuite;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import pa3.FindMinPath;
+import pa3.GraphNode;
+import pa3.GraphWrapper;
+
+import java.io.File;
+import java.io.FileReader;
+
+import static org.junit.Assert.assertTrue;
+import static pa3.FindMinPath.run;
 
 public class FindMinPathTest {
 
 	@Test
 	public void testShowPath() {
-		
+		GraphNode s = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a91", true);
+
+		GraphNode d  = new GraphNode("6bb67358-3761-455c-b283-2d309ca375e6", true);
+		d.previousNode = s;
+		d.previousDirection="East";
+		d.setEast(s,0);
+		String rst = FindMinPath.showPath(s,d,"");
+		assertTrue(rst.equals("West\n"));
+
+		d.previousDirection="West";
+		d.setWest(s,0);
+		rst = FindMinPath.showPath(s,d,"");
+		assertTrue(rst.equals("East\n"));
+
+		d.previousDirection="South";
+		d.setSouth(s,0);
+		rst = FindMinPath.showPath(s,d,"");
+		assertTrue(rst.equals("North\n"));
+
+		d.previousDirection="North";
+		d.setNorth(s,0);
+		rst = FindMinPath.showPath(s,d,"");
+		assertTrue(rst.equals("South\n"));
 	}
 
 	@Test
 	public void testRun() {
-		fail("Not yet implemented");
+		GraphWrapper gw = new GraphWrapper(false);
+		GraphNode home = gw.getHome();
+		String rst = run(gw,home);
+		System.out.println(rst);
+
+		String outputStr = "South\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"North\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"West\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"North\n" +
+				"East\n" +
+				"North\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"South\n" +
+				"South\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"South\n" +
+				"East\n" +
+				"East\n" +
+				"East\n" +
+				"East\n";
+		assertTrue(outputStr.equals(rst));
 	}
 
 	@Test
 	public void testWriteToFile() {
-		fail("Not yet implemented");
+		String writeStr = "test";
+		FindMinPath.writeToFile(writeStr);
+		File answer = new File("src/pa3/answer.txt");
+
+		try {
+			char cbuf[] = new char[4];
+			FileReader reader = new FileReader(answer);
+			reader.read(cbuf);
+			reader.close();
+			String rst = String.valueOf(cbuf);
+			assertTrue(writeStr.equals(rst));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}
 
 }
--- src/testSuite/HashmapTest.java
+++ src/testSuite/HashmapTest.java
@@ -67,6 +67,51 @@ public class HashmapTest {
 		test.set(node4, 2);
 		test.remove(node3);
 		assertTrue(test.hasKey(node3) == false);
+		GraphNode node6 = new GraphNode("6bb67358-3761-455c-b283-2d309ca375e8", true);
+		assertTrue(test.hasKey(node6) == true);
+	}
+
+	@Test
+	public void testRemoveForHashCrash() {
+		Hashmap test = new Hashmap(1);
+		GraphNode node1 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a91", true);
+		GraphNode node2 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a92", true);
+		GraphNode node3 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a93", true);
+		GraphNode node4 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a94", true);
+		GraphNode node5 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a95", true);
+		GraphNode node6 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a96", true);
+		GraphNode node7 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a97", true);
+		GraphNode node8 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a98", true);
+		GraphNode node9 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a99", true);
+		GraphNode node10 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a10", true);
+		GraphNode node11 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a11", true);
+		GraphNode node12 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a12", true);
+		test.set(node1, 1);
+		test.set(node2, 2);
+		test.set(node3, 2);
+		test.set(node4, 2);
+		test.set(node5, 2);
+		test.set(node6, 1);
+		test.set(node7, 2);
+		test.set(node8, 2);
+		test.set(node9, 2);
+		test.set(node10, 2);
+		test.set(node11, 1);
+		test.set(node12, 2);
+		test.remove(node1);
+		test.remove(node2);
+		test.remove(node3);
+		test.remove(node4);
+		test.remove(node5);
+		test.remove(node6);
+		test.remove(node7);
+		test.remove(node8);
+		test.remove(node9);
+		test.remove(node10);
+		test.remove(node11);
+		test.remove(node12);
+
+		assertTrue(test.hasKey(node3) == false);
 	}
 	
 	@Test
@@ -80,6 +125,36 @@ public class HashmapTest {
 		test.set(node2, 2);
 		test.set(node3, 2);
 		test.set(node4, 2);
+		test.remove(node4);
+		assertTrue(test.hasKey(node4) == false);
+	}
+
+	@Test
+	public void testRemoveForRemoveNull() {
+		Hashmap test = new Hashmap(5);
+		GraphNode node1 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a91", true);
+		GraphNode node2 = new GraphNode("6bb67358-3761-455c-b283-2d309ca375e6", true);
+		GraphNode node3 = new GraphNode("", true);
+		GraphNode node4 = new GraphNode("6bb67358-3761-455c-b283-2d309ca375e8", true);
+		test.set(node1, 1);
+		test.set(node2, 2);
+		test.set(node3, 2);
+		test.set(node4, 2);
+		test.remove(null);
+		assertTrue(test.hasKey(node4) == true);
+	}
+
+	@Test
+	public void testRemoveForRemoveNotExsit() {
+		Hashmap test = new Hashmap(5);
+		GraphNode node1 = new GraphNode("c4dbe07b-f0b5-4b8a-bf11-28780d609a91", true);
+		GraphNode node2 = new GraphNode("6bb67358-3761-455c-b283-2d309ca375e6", true);
+		GraphNode node3 = new GraphNode("", true);
+		GraphNode node4 = new GraphNode("6bb67358-3761-455c-b283-2d309ca375e8", true);
+		test.set(node1, 1);
+		test.set(node2, 2);
+		test.set(node3, 2);
+
 		test.remove(node4);
 		assertTrue(test.hasKey(node4) == false);
 	}
